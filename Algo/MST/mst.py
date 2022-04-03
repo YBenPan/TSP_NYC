3f89b6769d7342ca28fef449ab9d5a156ce80f4a
YBenPan <ybenp8104@outlook.com>
2022-04-02 22:32:17 -0400

    Import data

--- Algo/MST/mst.py
+++ Algo/MST/mst.py
@@ -1,4 +1,14 @@
-# See README.md for descrption
+# See README.md for description
+import os
+import numpy as np
+import pandas as pd
+
+# Import data
+data_path = os.path.abspath("../data")
+data_file = "/test1.csv"
+data = pd.read_csv(data_path + data_file, header=None)
+data = data.to_numpy()
+print(data)
 
 
 
