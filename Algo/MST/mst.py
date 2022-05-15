57d93c7a96bf38a729d9ea66cb4d6d6987fde7f9
Ben Pan <ybenp8104@outlook.com>
2022-05-15 08:24:46 -0400

    Convert to networkx graph

--- Algo/MST/mst.py
+++ Algo/MST/mst.py
@@ -2,13 +2,21 @@
 import os
 import numpy as np
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
 # Import data
 data_path = os.path.abspath("../data")
 data_file = "/test1.csv"
 data = pd.read_csv(data_path + data_file, header=None)
 data = data.to_numpy()
-print(data)
+
+graph = nx.from_numpy_array(data)
+pos = nx.spring_layout(graph)
+labels = nx.get_edge_attributes(graph, "weight")
+nx.draw(graph)
+nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+plt.show()
 
 
 
